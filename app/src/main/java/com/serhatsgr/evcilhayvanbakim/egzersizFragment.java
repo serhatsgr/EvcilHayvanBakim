b5a7b8f1c0be28fe9d4d610617112544016e5cb1
Serhat Sağır <95631951+serhatsgr@users.noreply.github.com>
2024-05-05 14:07:59 +0300

    Save Exercise Data to Firestore

--- app/src/main/java/com/serhatsgr/evcilhayvanbakim/egzersizFragment.java
+++ app/src/main/java/com/serhatsgr/evcilhayvanbakim/egzersizFragment.java
@@ -1,6 +1,7 @@
 package com.serhatsgr.evcilhayvanbakim;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,31 +13,50 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
-public class EgzersizFragment extends Fragment {
+public class egzersizFragment extends Fragment {
 
-    private EditText etExerciseType;
-    private EditText etExerciseDuration;
-    private EditText etExerciseDate;
-    private EditText etExerciseTime;
+     TextInputEditText etExerciseType,etExerciseDuration,etExerciseDate,etExerciseTime;
 
+     Button btn;
+
+
+
+    //firebase
+    private FirebaseAuth mAuth;
+
+    private FirebaseUser mUsers;
+
+    private HashMap<String, Object> mData;
+
+    private FirebaseFirestore mFireStore;
+
+    //chart
     private Calendar calendar;
     private int year, month, dayOfMonth, hourOfDay, minute;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         View view = inflater.inflate(R.layout.fragment_egzersiz, container, false);
 
 
@@ -47,6 +67,10 @@ public class EgzersizFragment extends Fragment {
         etExerciseDuration = view.findViewById(R.id.etExerciseDuration);
         etExerciseDate = view.findViewById(R.id.etExerciseDate);
         etExerciseTime = view.findViewById(R.id.etExerciseTime);
+        btn= view.findViewById(R.id.btnadd);
+
+        mAuth=FirebaseAuth.getInstance();
+        mFireStore=FirebaseFirestore.getInstance();
 
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
@@ -54,6 +78,67 @@ public class EgzersizFragment extends Fragment {
         dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
         hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
         minute = calendar.get(Calendar.MINUTE);
+
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String type, duration, date, time;
+
+                type = String.valueOf(etExerciseType.getText());
+                duration = String.valueOf(etExerciseDuration.getText());
+                date = String.valueOf(etExerciseDate.getText());
+                time = String.valueOf(etExerciseTime.getText());
+
+                if (TextUtils.isEmpty(type)) {
+                    Toast.makeText(getActivity(), "Egzersiz Türü Giriniz.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(duration)) {
+                    Toast.makeText(getActivity(), "Egzersiz Süresi Giriniz.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(date)) {
+                    Toast.makeText(getActivity(), "Egzersiz Tarihi Giriniz.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(time)) {
+                    Toast.makeText(getActivity(), "Egzersiz Zamanı Giriniz.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mUsers = mAuth.getCurrentUser();
+                if (mUsers != null) {
+                    String userId = mUsers.getUid();
+
+                    mData = new HashMap<>();
+                    mData.put("type", type);
+                    mData.put("duration", duration);
+                    mData.put("date", date);
+                    mData.put("time", time);
+                    mData.put("userId", userId);
+
+                    mFireStore.collection("exercise")
+                            .add(mData)
+                            .addOnSuccessListener(documentReference -> {
+                                Toast.makeText(getActivity(), "Egzersiz bilgisi kaydedildi.", Toast.LENGTH_SHORT).show();
+                            })
+                            .addOnFailureListener(e -> {
+                                Toast.makeText(getActivity(), "Egzersiz bilgisi kaydedilirken bir hata oluştu.", Toast.LENGTH_SHORT).show();
+                            });
+                            } else {
+                                Toast.makeText(getActivity(), "Kullanıcı oturumu açılmamış.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
 
         etExerciseDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,8 +169,6 @@ public class EgzersizFragment extends Fragment {
         });
 
         return view;
-
-
 
     }
 
