f39ba78010f85f65937a1ff391e7c863c46fd0b6
Serhat Sağır <95631951+serhatsgr@users.noreply.github.com>
2024-05-06 18:56:14 +0300

    vaccination information saving to Firestore in app

--- app/src/main/java/com/serhatsgr/evcilhayvanbakim/asiSayfa.java
+++ app/src/main/java/com/serhatsgr/evcilhayvanbakim/asiSayfa.java
@@ -3,48 +3,108 @@ package com.serhatsgr.evcilhayvanbakim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class asiSayfa extends AppCompatActivity {
 
-    private EditText etVaccineName, etVaccineDate;
+    private TextInputEditText etVaccineName, etVaccineDate;
     private Button btnAddVaccine;
     private ListView lvVaccines;
     private List<String> vaccineList;
     private ArrayAdapter<String> vaccineAdapter;
 
+    private FirebaseAuth mAuth;
+
+    private FirebaseUser mUsers;
+
+    private HashMap<String, Object> mData;
+
+    private FirebaseFirestore mFireStore;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_asi_sayfa);
 
-        etVaccineName = findViewById(R.id.etVaccineName);
-        etVaccineDate = findViewById(R.id.etVaccineDate);
-        btnAddVaccine = findViewById(R.id.btnAddVaccine);
-        lvVaccines = findViewById(R.id.lvVaccines);
+        etVaccineName=findViewById(R.id.etVaccineName);
+        etVaccineDate=findViewById(R.id.etVaccineDate);
+        btnAddVaccine=findViewById(R.id.etVaccineDate);
 
-        vaccineList = new ArrayList<>();
-        vaccineAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, vaccineList);
-        lvVaccines.setAdapter(vaccineAdapter);
+        mAuth=FirebaseAuth.getInstance();
+        mFireStore=FirebaseFirestore.getInstance();
 
         btnAddVaccine.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String vaccineName = etVaccineName.getText().toString();
-                String vaccineDate = etVaccineDate.getText().toString();
-                String vaccineInfo = vaccineName + " - " + vaccineDate;
-                vaccineList.add(vaccineInfo);
-                vaccineAdapter.notifyDataSetChanged();
-                etVaccineName.setText("");
-                etVaccineDate.setText("");
+            public void onClick(View v) {
+               String vaccineName,date;
+
+               vaccineName=String.valueOf(etVaccineDate.getText());
+               date=String.valueOf(etVaccineDate.getText());
+
+               if(TextUtils.isEmpty(vaccineName)){
+                   Toast.makeText(asiSayfa.this, "Egzersiz Türü Giriniz.", Toast.LENGTH_SHORT).show();
+               }
+               if(TextUtils.isEmpty(date)){
+                   Toast.makeText(asiSayfa.this,"Aşı olma tarihini giriniz!",Toast.LENGTH_SHORT).show();
+               }
+
+               mUsers=mAuth.getCurrentUser();
+               if(mUsers!=null){
+                 String userId=mUsers.getUid();
+
+                 mFireStore.collection("vaccines")
+                         .whereEqualTo("userId",userId)
+                         .get()
+                         .addOnSuccessListener(querySnapshots -> {
+                           if (querySnapshots.size()>=30){
+                                Toast.makeText(asiSayfa.this,"Ekleme yapmadan önce bazı eski aşı kayıtlarını sil!",Toast.LENGTH_SHORT).show();
+                           }
+                           else{
+                               mData=new HashMap<>();
+                               mData.put("type",vaccineName);
+                               mData.put("date",date);
+
+                               mFireStore.collection("vaccines")
+                                       .add(mData)
+                                       .addOnSuccessListener(documentReference -> {
+                                           Toast.makeText(asiSayfa.this,"Aşı başarıyla kaydedildi.",Toast.LENGTH_SHORT).show();
+
+                                       })
+
+                                       .addOnFailureListener(e -> {
+                                           Toast.makeText(asiSayfa.this,"Aşı kaydedilemedi.",Toast.LENGTH_SHORT).show();
+                                       });
+                           }
+
+                          })
+                         .addOnFailureListener(e -> {
+                             Toast.makeText(asiSayfa.this,"Aşı bilgileri alınırken hata oluştu.",Toast.LENGTH_SHORT).show();
+                         });
+
+               }
+
+               else {
+                   Toast.makeText(asiSayfa.this,"Kullanıcı oturumu açılmamış.",Toast.LENGTH_SHORT).show();
+               }
             }
         });
+
+
     }
 }
